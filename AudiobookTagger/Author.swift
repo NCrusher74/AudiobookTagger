60904cb5aac7bc2b47e93cd287a2173c10f7cbf9
NCrusher74 <nolainecrusher@gmail.com>
2020-02-28 11:22:45 -0800

    trying to fix missing UI test issue

--- AudiobookTagger/Author.swift
+++ AudiobookTagger/Author.swift
@@ -10,7 +10,11 @@ import Foundation
 
 struct Author {
     
-    
+//    var read: String {
+//        
+//        
+//        
+//    }
     
     
 }
--- AudiobookTagger/CommandBuilder.swift
+++ AudiobookTagger/CommandBuilder.swift
@@ -11,28 +11,27 @@ import ID3TagEditor
 import MP42Foundation
 
 /// The output array is the array of tags that will be written to the output file.
-struct ArrayBuilder {
+struct CommandBuilder {
     var format: AudiobookType
     
     init(format: AudiobookType) {
         self.format = format
     }
     
-    
+    /// the array of ID3 frames being accessed
     var id3FrameArray: [FrameName: ID3Frame] = [:]
     
-    /// the array of ID3 frames being accessed
-    func mp3Array(input: String, output: String) {
-        _ = ID3TagEditor()
+    /// the command that goes into ID3Tag.read or ID3tag.write
+    func mp3Array() {
         _ = ID3Tag(
             version: .version3, frames: id3FrameArray)
     }
     
-   
+    /// the array of MP4 items being accessed
     var mp4MetadataArray: [MP42MetadataItem] = []
     
-     /// the array of MP4 frames being accessed
-    func mp4Array(input: URL, output: URL) throws {
+    /// the command to access those items for reading or writng
+    func mp4Array(input: URL) throws {
         let mp42File = try MP42File(url: input)
         mp42File.metadata.addItems(mp4MetadataArray)
     }
