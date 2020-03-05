82b36872de880f0e9fbb26b6435d5a37dbb357c1
NCrusher74 <nolainecrusher@gmail.com>
2020-03-05 10:24:55 -0800

    trouble iterating over Outcast frames

---
+++ AudiobookTagger/Chapters.swift
@@ -0,0 +1,21 @@
+//
+//  AudiobookChaptering.swift
+//  AudiobookTagger
+//
+//  Created by Nolaine Crusher on 3/5/20.
+//  Copyright © 2020 Nolaine Crusher. All rights reserved.
+//
+
+import Foundation
+import MP42Foundation
+import OutcastID3
+
+enum Chapters {
+    case list
+    case rename
+    case delete
+    case add
+    
+    
+    //var id3Chapter: OutcastID3TagFrame
+}
--- AudiobookTaggerTests/ChapteringTests.swift
+++ AudiobookTaggerTests/ChapteringTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import MP42Foundation
+import OutcastID3
 @testable import AudiobookTagger
 
 class MP4ChapteringTests: XCTestCase {
@@ -27,4 +28,43 @@ class MP4ChapteringTests: XCTestCase {
         let outputUrl = URL(fileURLWithPath: (NSHomeDirectory() + "/MP42Foundation-testchapters.m4b"))
          XCTAssertNoThrow(try mp4File.write(to: outputUrl, options: nil))
     }
+}
+
+
+class OutcastID3Tests: XCTestCase {
+    
+    func testID3ChaptersListing() throws {
+        let url = Bundle.testMp3ChapteredBlank
+        let outcastFile = try OutcastID3.MP3File(localUrl: url)
+        
+        let outcastTag = try outcastFile.readID3Tag()
+        print("TAG: \(outcastTag)")
+        
+        let outcastFrames = try outcastFile.readID3Tag().tag.frames
+        print("FRAMES: \(outcastFrames)")
+        
+        for frame in outcastFrames {
+            print("FRAME: \(frame)")
+        }
+        
+        for frame in outcastFrames {
+            if frame.contains("elementID=TOC") {
+                print("TOC: \(frame)")
+            }
+        }
+        
+        XCTAssertEqual(1,1)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
