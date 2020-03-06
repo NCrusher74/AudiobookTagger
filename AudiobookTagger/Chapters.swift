703c79ce4bef1d45f667620f7932db235e6495bb
NCrusher74 <nolainecrusher@gmail.com>
2020-03-06 07:10:53 -0800

    coming along...

--- AudiobookTagger/Chapters.swift
+++ AudiobookTagger/Chapters.swift
@@ -10,12 +10,105 @@ import Foundation
 import MP42Foundation
 import OutcastID3
 
-enum Chapters {
-    case list
-    case rename
-    case delete
-    case add
+struct Chapters {
     
+    var audiobookFile: AudiobookFile
     
-    //var id3Chapter: OutcastID3TagFrame
+    init(for audiobookFile: AudiobookFile) {
+        self.audiobookFile = audiobookFile
+    }
+    
+    /// accesses the ID3 `TableOfContents` frame
+    var id3TocFrame: OutcastID3.Frame.TableOfContentsFrame? {
+        do {
+            let outcastFile = try OutcastID3.MP3File(localUrl: audiobookFile.audiobookUrl)
+            let outcastTag = try outcastFile.readID3Tag()
+            for frame in outcastTag.tag.frames {
+                if let toc = frame as? OutcastID3.Frame.TableOfContentsFrame {
+                    return toc
+                }
+            }
+        } catch { print("unable to read table of contents frame", error, error.localizedDescription)}
+        ; return nil
+    }
+
+    /// accesses the ID3 `ChapterFrame`
+    var id3ChapterFrame: OutcastID3.Frame.ChapterFrame? {
+        do {
+            let outcastFile = try OutcastID3.MP3File(localUrl: audiobookFile.audiobookUrl)
+            let outcastTag = try outcastFile.readID3Tag()
+            for frame in outcastTag.tag.frames {
+                if let chapter = frame as? OutcastID3.Frame.ChapterFrame {
+                    return chapter
+                }
+            }
+        } catch { print("unable to read chapter frame", error, error.localizedDescription)}
+        ; return nil
+    }
+    
+    func chapterData() throws -> [String: Any] {
+        switch audiobookFile.format {
+            /// returns date for mp3 chapters:
+            /// `chapterIndex`,
+            /// `chapterTitle`,
+            /// `elementID`,
+            /// `chapterStartTime`,
+            /// `chapterEndTime`,
+            /// `chapterStartByteOffset`
+            /// `chapterEndByteOffset`
+            case .mp3 :
+                var chapterElementsDictionary: [String:Any] = [:]
+                let outcastFile = try OutcastID3.MP3File(
+                    localUrl: audiobookFile.audiobookUrl)
+                let outcastFrames = try outcastFile.readID3Tag().tag.frames
+                var index = 0
+                for frame in outcastFrames {
+                    if let chapterFrame = frame as? OutcastID3.Frame.ChapterFrame {
+                        let chapterIndex = index
+                        chapterElementsDictionary["chapterIndex (for frame at \(index))"] = chapterIndex
+                        chapterElementsDictionary["elementID (for frame at \(index))"] = chapterFrame.elementId
+                        chapterElementsDictionary["chapterStartTime (for frame at \(index))"] = chapterFrame.startTime
+                        chapterElementsDictionary["chapterEndTime for frame at \(index)"] = chapterFrame.endTime
+                        chapterElementsDictionary["chapterStartByteOffset for frame at \(index)"] = chapterFrame.startByteOffset
+                        chapterElementsDictionary["chapterEndByteOffset for frame at \(index)"] = chapterFrame.endByteOffset
+                        let subFrames = chapterFrame.subFrames
+                        for frame in subFrames {
+                            if let chapterTitle = frame as? OutcastID3.Frame.StringFrame {
+                                chapterElementsDictionary["chapterTitle"] = chapterTitle.str
+                            }
+                        }
+                        index += 1
+                    }
+                }
+            /// returns index, title, and timestamp for MP4 frame
+            case .mp4 :
+                var chapterElementsDictionary: [String: Any] = [:]
+                let mp4File = try MP42File(url: audiobookFile.audiobookUrl)
+                let chapterTrack = mp4File.chapters
+                let chapterCountAsInt = chapterTrack?.chapterCount() ?? 0
+                let chapterIndex = 0..<chapterCountAsInt
+                for i in chapterIndex {
+                    chapterElementsDictionary["chapterIndex"] = i
+                    chapterElementsDictionary["chapterTitle"] = mp4File.chapters?.chapter(
+                        at: UInt(i)).title ?? ""
+                    chapterElementsDictionary["chapterStartInMilliseconds"] =
+                        (mp4File.chapters?.chapter(at: UInt(i)).timestamp) ?? 0
+                    return chapterElementsDictionary
+                }
+            
+            case .invalid :
+                throw AudiobookFile.Error.unknownFileFormat
+        }; return [:]
+    }
+    
+//    func renameChapter() {
+//        switch audiobookFile.format {
+//            case .mp3
+//            
+//            case .mp4
+//            
+//            case .invalid
+//        }
+//    }
+    
 }
--- AudiobookTaggerTests/ChapteringTests.swift
+++ AudiobookTaggerTests/ChapteringTests.swift
@@ -12,52 +12,82 @@ import OutcastID3
 @testable import AudiobookTagger
 
 class MP4ChapteringTests: XCTestCase {
-
+    
     func testMP4Chapters() throws {
         let mp4File = try MP42File(url: Bundle.testM4bChapteredBlank)
         let chapterTrack = mp4File.chapters
-
-        //print(chapterTrack?.chapterCount())
-        chapterTrack?.removeChapters(at: [0,1,2,3])
-        chapterTrack?.addChapter("ch95", duration: 0)
-        chapterTrack?.addChapter("ch96", duration: 7)
-        chapterTrack?.addChapter("ch97", duration: 14)
-        chapterTrack?.addChapter("ch98", duration: 21)
-        chapterTrack?.addChapter("ch99", duration: 28)
-        
+        let chapterCountAsInt = chapterTrack?.chapterCount() ?? 0
+        let chapterIndex = 0..<chapterCountAsInt
+        for i in chapterIndex {
+            print(i)
+            print(mp4File.chapters?.chapter(at: UInt(i)).title ?? "")
+            print((mp4File.chapters?.chapter(at: UInt(i)).timestamp) ?? 0)
+        }
+        let chapterZed = mp4File.chapters?.chapter(at: 0)
+        mp4File.chapters?.setTitle("BobsUrUncle", forChapter: chapterZed!)
         let outputUrl = URL(fileURLWithPath: (NSHomeDirectory() + "/MP42Foundation-testchapters.m4b"))
-         XCTAssertNoThrow(try mp4File.write(to: outputUrl, options: nil))
+        XCTAssertNoThrow(try mp4File.write(to: outputUrl, options: nil))
     }
 }
+        
+        
+
+        
+
+        
+//        chapterTrack?.removeChapters(at: [0,1,2,3])
+//        chapterTrack?.addChapter("ch95", duration: 0)
+//        chapterTrack?.addChapter("ch96", duration: 7)
+//        chapterTrack?.addChapter("ch97", duration: 14)
+//        chapterTrack?.addChapter("ch98", duration: 21)
+//        chapterTrack?.addChapter("ch99", duration: 28)
+//
+
 
 
 class OutcastID3Tests: XCTestCase {
     
     func testID3ChaptersListing() throws {
-        let url = Bundle.testMp3ChapteredBlank
-        let outcastFile = try OutcastID3.MP3File(localUrl: url)
-        
-        let outcastTag = try outcastFile.readID3Tag()
-        print("TAG: \(outcastTag)")
-        
+        var chapterElementsDictionary: [String:Any] = [:]
+        let outcastFile = try OutcastID3.MP3File(
+            localUrl: Bundle.testMp3ChapteredBlank)
         let outcastFrames = try outcastFile.readID3Tag().tag.frames
-        print("FRAMES: \(outcastFrames)")
-        
+        var index = 0
         for frame in outcastFrames {
-            print("FRAME: \(frame)")
+            if let chapterFrame = frame as? OutcastID3.Frame.ChapterFrame {
+                let chapterIndex = index
+                chapterElementsDictionary["chapterIndex (for frame at \(index))"] = chapterIndex
+                chapterElementsDictionary["elementID (for frame at \(index))"] = chapterFrame.elementId
+                chapterElementsDictionary["chapterStartTime (for frame at \(index))"] = chapterFrame.startTime
+                chapterElementsDictionary["chapterEndTime for frame at \(index)"] = chapterFrame.endTime
+                chapterElementsDictionary["chapterStartByteOffset for frame at \(index)"] = chapterFrame.startByteOffset
+                chapterElementsDictionary["chapterEndByteOffset for frame at \(index)"] = chapterFrame.endByteOffset
+                let subFrames = chapterFrame.subFrames
+                for frame in subFrames {
+                    if let chapterTitle = frame as? OutcastID3.Frame.StringFrame {
+                        chapterElementsDictionary["chapterTitle"] = chapterTitle.str
+                    }
+                }
+                index += 1
+            }; print(chapterElementsDictionary)
         }
+
         
-        for frame in outcastFrames {
-            if frame.contains("elementID=TOC") { //"Value of type 'OutcastID3TagFrame' has no member 'contains'"
-                print("TOC: \(frame)")
-            }
-        }
-        
-        XCTAssertEqual(1,1)
     }
     
-    
-    
+    func testGetFrameIndex() throws {
+        let url = Bundle.testMp3ChapteredBlank
+        let outcastFile = try OutcastID3.MP3File(localUrl: url)
+        var outcastTag = try outcastFile.readID3Tag()
+        var outcastFrames = outcastTag.tag.frames
+        var index = 0
+        for frame in outcastFrames {
+            index += 1
+            if let chapterFrame = frame as? OutcastID3.Frame.ChapterFrame {
+                print("Chapter \(index) Index = \(index)")
+            }
+        }
+    }
     
     
     
