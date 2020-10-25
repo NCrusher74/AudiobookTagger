ceb9018391003dab3c62fb2e7db4a7e64e2cff27
Nolaine Crusher <nolainecrusher@gmail.com>
2020-10-25 13:20:28 -0700

    made type enum public

--- Sources/AudiobookTagger/AudiobookType.swift
+++ Sources/AudiobookTagger/AudiobookType.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-enum AudiobookType: String, CaseIterable {
+public enum AudiobookType: String, CaseIterable {
     case narrated = "Narrated"
     case fullCast = "Full cast"
     case graphicAudio = "Graphic Audio"
